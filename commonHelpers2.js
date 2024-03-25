c002663f575c409b8be088975650c0a3037e3a74
Christina-Mikheyeva <99538274+Christina-Mikheyeva@users.noreply.github.com>
2024-03-25 12:38:13 +0000

    Deploying to gh-pages from @ Christina-Mikheyeva/goit-js-hw-09@b75b6ee04e098ea292ce911d712b10e8e08e9081 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,15 @@
+import"./assets/styles-8690beac.js";const l=document.querySelector("section"),c=`
+<form class="feedback-form" autocomplete="off">
+    <label>
+        Email
+        <input type="email" name="email" autofocus />
+    </label>
+
+    <label>
+        Message
+        <textarea name="message" rows="8"></textarea>
+    </label>
+
+    <button type="submit">Submit</button>
+</form>`;l.insertAdjacentHTML("beforeend",c);const r=document.querySelector(".feedback-form"),a=document.querySelector("input"),o=document.querySelector("textarea"),n="feedback-form-state";r.addEventListener("input",u);r.addEventListener("submit",i);function u(){const e=a.value,t=o.value,s=JSON.stringify({input:e,textarea:t});localStorage.setItem(n,s)}function m(){const e=localStorage.getItem(n),t=JSON.parse(e);t&&(a.value=t.input,o.value=t.textarea)}m();function i(e){a.value===""||o.value===""?alert("Fill all fields"):(e.preventDefault(),alert("Thanks, form send!"),e.currentTarget.reset(),localStorage.removeItem(n))}
+//# sourceMappingURL=commonHelpers2.js.map
