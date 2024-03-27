e740192d63b3deba0fddc1b080d7bba25f99be96
Christina-Mikheyeva <99538274+Christina-Mikheyeva@users.noreply.github.com>
2024-03-27 21:35:23 +0000

    Deploying to gh-pages from @ Christina-Mikheyeva/goit-js-hw-09@a4b892a619a483dcd0d997193364026e8afd8e64 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,15 +1,2 @@
-import"./assets/styles-44dfdf90.js";const l=document.querySelector("section"),c=`
-<form class="feedback-form" autocomplete="off">
-    <label>
-        Email
-        <input type="email" name="email" autofocus />
-    </label>
-
-    <label>
-        Message
-        <textarea name="message" rows="8"></textarea>
-    </label>
-
-    <button type="submit">Submit</button>
-</form>`;l.insertAdjacentHTML("beforeend",c);const r=document.querySelector(".feedback-form"),a=document.querySelector("input"),o=document.querySelector("textarea"),n="feedback-form-state";r.addEventListener("input",u);r.addEventListener("submit",i);function u(){const e=a.value,t=o.value,s=JSON.stringify({input:e,textarea:t});localStorage.setItem(n,s)}function m(){const e=localStorage.getItem(n),t=JSON.parse(e);t&&(a.value=t.input,o.value=t.textarea)}m();function i(e){a.value===""||o.value===""?alert("Fill all fields"):(e.preventDefault(),alert("Thanks, form send!"),e.currentTarget.reset(),localStorage.removeItem(n))}
+import"./assets/styles-44dfdf90.js";const n=document.querySelector(".feedback-form"),a=document.querySelector("input"),r=document.querySelector("textarea"),o="feedback-form-state";n.addEventListener("input",s);n.addEventListener("submit",i);function s(){const t={email:a.value.trim(),message:r.value.trim()},e=JSON.stringify(t);localStorage.setItem(o,e)}function l(){const t=localStorage.getItem(o),e=JSON.parse(t);e&&(a.value=e.input.trim(),r.value=e.textarea.trim())}l();function i(t){t.preventDefault();const e={Email:a.value.trim(),Message:r.value.trim()};a.value===""||r.value===""?alert("Fill all fields"):(alert("Thanks, form send!"),console.log(e),t.currentTarget.reset(),localStorage.removeItem(o))}
 //# sourceMappingURL=commonHelpers2.js.map
